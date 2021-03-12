ae2554cc05f8a03ce1c153579c341dc9dcc4575f
TypicalTalon <71259829+TypicalTalon@users.noreply.github.com>
2021-03-12 13:52:21 +0100

    Initial commit

---
+++ Gebäude/Gebäude/QuadratMeterConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Gebäude
+{
+    class QuadratMeterConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double quadratmeter = 0;
+
+            quadratmeter = ((double)values[0]) * ((double)values[1]) * (double)((int)values[2]+1);
+            
+            return $"{Math.Round(quadratmeter,2)} m²";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gebäude/Gebäude/XCoordinateConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Gebäude
+{
+    class XCoordinateConverter : IMultiValueConverter
+    {
+      
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double temp = 0;
+            if (values[0] != null)
+            {
+                temp = (double)values[1] - (double)values[0]; 
+            }
+            return temp;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
